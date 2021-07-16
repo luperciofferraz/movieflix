d2c971ea66fade871486f3a9ca420814e261ee9c
Lupércio <lupercio.ferraz@gmail.com>
2021-07-16 17:53:27 -0300

    Production configuration

--- backend/src/main/java/com/devsuperior/movieflix/entities/Movie.java
+++ backend/src/main/java/com/devsuperior/movieflix/entities/Movie.java
@@ -4,6 +4,7 @@ import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -31,6 +32,7 @@ public class Movie implements Serializable {
 	
 	private String imgUrl;
 	
+	@Column(columnDefinition = "TEXT")
 	private String synopsis;
 	
 	@ManyToOne
